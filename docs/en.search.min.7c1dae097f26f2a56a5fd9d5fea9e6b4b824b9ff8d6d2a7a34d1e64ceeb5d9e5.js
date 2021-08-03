1d40bd6352df6a5e6c81e7c57c42118b2e092597
BrynMarieR <bryn.marie.r@gmail.com>
2021-08-03 17:31:19 -0400

    change publish dir and base url

--- docs/en.search.min.7c1dae097f26f2a56a5fd9d5fea9e6b4b824b9ff8d6d2a7a34d1e64ceeb5d9e5.js
+++ docs/en.search.min.7c1dae097f26f2a56a5fd9d5fea9e6b4b824b9ff8d6d2a7a34d1e64ceeb5d9e5.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/brynmarier.github.io/ebook-site/flexsearch.min.js');loadScript('/brynmarier.github.io/ebook-site/en.search-data.min.68afee672afd874ca383df83cae55ee85b89c110e4abd8c0467f9e94ff583922.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/ebook-site/flexsearch.min.js');loadScript('/ebook-site/en.search-data.min.0b749f6d7c8a21a74358fbdd48f01819023f7f4c1724f63b908a105c73606184.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
